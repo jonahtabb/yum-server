8e6395307e8e320650738426c3039a5c8aa1366c
Hannah <hannahtoth@Hannahs-MacBook-Pro-2.local>
2021-07-10 13:02:36 -0400

    adding create endpoint'

---
+++ controllers/cookbookcontroller.js
@@ -0,0 +1,35 @@
+
+let Express = require('express');
+let router = Express.Router();
+
+//Import the Cookbook Model
+const { CookbookModel } = require('../models');
+
+
+/*
+================
+Cookbook Create
+================
+*/
+
+router.post('/create', async (req, res) => {
+    const {recipeName, image, source, url, ingredients, notes} = req.body.cookbook;
+    const CookbookEntry = {
+        recipeName,
+        image,
+        source,
+        url,
+        ingredients,
+        notes
+    }
+    try {
+        const newCookbook = await CookbookModel.create(CookbookEntry);
+        res.status(200).json({newCookbook});
+    } catch (err) {
+        res.status(500).json({ error : err});
+    }
+    
+});
+
+
+module.exports = router;
