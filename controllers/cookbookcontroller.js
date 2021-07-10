73da0a4b7a15341e56169d821dfed8b9fd109e45
Miguel Caballero <cp0251963@gmail.com>
2021-07-10 14:04:57 -0400

    get all recipes and delete updated

--- controllers/cookbookcontroller.js
+++ controllers/cookbookcontroller.js
@@ -1,30 +1,8 @@
 let Express = require('express');
 let router = Express.Router();
 let validateJWT = require('../middleware/validate-jwt');
-//Import the CookBook Model
-const { CookBookModel } = require('../models');
-
-/*
-==================
-CookBook Create
-==================
-*/
-router.post('/create', validateJWT, async (req, res) => {
-  const { cuisine, category, ingridients } = req.body.cookbook;
-  const { id } = req.user;
-  const cookbookEntry = {
-    cuisine,
-    category,
-    ingridients,
-    owner: id,
-  };
-  try {
-    const newCookBook = await CookBookModel.create(cookbookEntry);
-    res.status(200).json(newCookBook);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
+//Import the Cookbook Model
+const { CookbookModel } = require('../models');
 
 /*
 ====================
@@ -32,107 +10,40 @@ Get all cookbook logs
 ====================
 */
 
-router.get('/', async (req, res) => {
+router.get('/getall', async (req, res) => {
+  //console.log('entries');
   try {
-    const entries = await CookBookModel.findAll();
+    const entries = await CookbookModel.findAll();
+
     res.status(200).json(entries);
   } catch (err) {
+    //console.log(err);
     res.status(500).json({ error: err });
   }
 });
 
-/*
-=============================
-Get CookBooks by owner
-=============================
-*/
-router.get('/mine', validateJWT, async (req, res) => {
-  const { id } = req.user;
-  try {
-    const userCookBook = await WorkoutlogModel.findAll({
-      where: {
-        owner: id,
-      },
-    });
-    res.status(200).json(userCookBook);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-/*
-==============================
-Get CookBooks by cuisine
-==============================
-*/
-router.get('/:cuisine', async (req, res) => {
-  const { cuisine } = req.params;
-  try {
-    const desc = await CookBookModel.findAll({
-      where: { cuisine: cuisine },
-    });
-    res.status(200).json(ingridients);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-/*
-====================
-Update a CookBook
-====================
-*/
-router.put('/update/:resultsId', validateJWT, async (req, res) => {
-  const { cuisine, category, ingridients } = req.body.workoutlog;
-  const CookBookId = req.params.resultsId;
-  const userId = req.user.id;
-
-  const query = {
-    where: {
-      id: CookBookId,
-      owner: userId,
-    },
-  };
-
-  const updateCookBook = {
-    cuisine: cuisine,
-    category: category,
-    ingridients: ingridients,
-  };
-
-  try {
-    const update = await CookBookModel.update(updatedCookBook, query);
-    res.status(200).json(update);
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-/*
-====================
-Delete a Recipe
-====================
-*/
-router.delete('/delete/:id', validateJWT, async (req, res) => {
-  const ownerId = req.user.id;
-  const CookBookId = req.params.id;
+// /*
+// ====================
+// Delete a Recipe
+// ====================
+// */
+router.delete('/delete/:id', async (req, res) => {
+  //const ownerId = req.user.id;
+  const CookbookId = req.params.id;
 
   try {
     const query = {
       where: {
-        id: CookBookId,
-        owner: ownerId,
+        id: CookbookId,
+        //owner: ownerId,
       },
     };
 
-    await WorkoutlogModel.destroy(query);
-    res.status(200).json({ message: 'Recipe Entry Removed' });
+    await CookbookModel.destroy(query);
+    res.status(200).json({ message: 'Recipe removed' });
   } catch (err) {
     res.status(500).json({ error: err });
   }
 });
-//router.get('/about', (req, res) => {
-//res.send('This is the about route');
-//});
 
 module.exports = router;
