8feb6afffa32759dd873124081cacd1dd26959dc
jonahtabb <jonahtabb@gmail.com>
2021-07-10 20:45:16 -0400

    added headers middleware for cors handling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dbConnection = require('./db');
 const controllers = require('./controllers');
 
 app.use(Express.json());
-
+app.use(require("./middleware/headers"))
 app.use('/user', controllers.userController);
 
 app.use(require("./middleware/validate-jwt"))
